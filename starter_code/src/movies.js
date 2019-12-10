bac0f82e3a4d28640bbc39c49edb197987a55977
meji <soy@meji.es>
2019-12-10 21:59:06 +0100

    inicio de ejercicio

---
+++ starter_code/src/movies.js
@@ -0,0 +1,14 @@
+const orderByYear = (array)=>{
+    return array.sort((a,b) =>{
+        if(a.year>b.year) return 1
+        if(a.year<b.year) return -1
+        else {
+            if(a.title>b.title) return 1
+            if(a.title<b.title) return -1
+        }   
+    })
+}
+
+const howManyMovies = (arr, selectedGenre, selectedDirector ) => {
+    return (arr.filter(movie => ((movie.genre===selectedGenre)&&(movie.director ===selectedDirector))).length
+}
